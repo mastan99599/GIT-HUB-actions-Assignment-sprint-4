29d91c2229d7e48d8287ab9bf3590ed1689bf4cf
Shaik Masthan <55420697+mastan99599@users.noreply.github.com>
2022-09-17 12:29:36 +0530

    new commit of module

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
-resource "aws_instance" "shaikec2demo" {
-  
-  ami            = var.ami_name
-  instance_type  = var.instance_type
-  tags           = var.tags
+module "ec2" {
+  source             = "../ec2"
+  for_each           = var.shaik_loop
+  ami_name           = each.value.ami
+  instance_type      = each.value.instance_type
+  tags               = each.value.tags
+
 }
+
